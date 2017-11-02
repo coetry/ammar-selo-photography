480486ec3d93fa3ad47d432b4c66a96c55e68313
Allen Hai <hai@growinfinitely.com>
2017-11-01 19:58:24 -0500

    got slider to loop once

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -11,27 +11,37 @@ class Slider extends Component {
         {'imageUrl':'https://images.unsplash.com/photo-1462212018902-1ca14eec1bb2?auto=format&fit=crop&w=750&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D'},
         {'imageUrl':'https://images.unsplash.com/photo-1496264057429-6a331647b69e?auto=format&fit=crop&w=751&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D'}
       ],
-      current: 2
+      current: 0
     }
   }
 
+  componentDidMount() {
+    window.setInterval(this.changeCurrent.bind(this), 2000)
+  }
+
+  changeCurrent () {
+    let current = this.state.current;
+    let imageArray = this.state.background.length -1;
+
+    if((current >= 0) && (current !== imageArray)) {
+         this.setState({ current: current + 1 })
+       if(current === imageArray) {
+         this.setState({ current: 0 })
+       }
+     }
+
+  }
 
   render() {
     return (
       <div className="slideshow">
-        <Slide background={this.state.background}
-               current={this.state.current} />
+        <Slide
+          background={this.state.background}
+          current={this.state.current} />
         <style jsx>{`
           .slideshow {
             overflow: hidden;
           }
-
-          .slide {
-            height: 100vh;
-            background-image: url('https://images.unsplash.com/photo-1466854076813-4aa9ac0fc347?dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D');
-            background-size: cover;
-          }
-
         `}</style>
       </div>
      )
