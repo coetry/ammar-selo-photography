a8ab7030f8ee3a77b9ed7046acf16f5a841ebc8d
Allen Hai <hai@growinfinitely.com>
2017-11-02 17:05:56 -0500

    ken burns in motion baby

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -3,15 +3,35 @@ import React from 'react'
 function Slide ({background, current}) {
   return (
     <div>
-    <div className="slide"></div>
-    <style jsx>{`
-      div {
-        height: 100vh;
-        background-image: url('${background[current].imageUrl}');
-        background-size: cover;
-        background-position: center;
-      }
-    `}</style>
+      <div className="slide"></div>
+      <style jsx>{`
+        .slide {
+          height: 100vh;
+          background-image: url('${background[current].imageUrl}');
+          background-size: cover;
+          background-position: center;
+          animation: kenburns 20s infinite;
+        }
+
+        @keyframes kenburns {
+          0% {
+            opacity: 0;
+          }
+          5% {
+            opacity: 1;
+          }
+          95% {
+            transform: scale3d(1.5,1.5,1.5);
+            animation-timing-function: ease-in;
+            opacity: 1;
+          }
+          100%{
+            transform: scale3d(2,2,2);
+            opacity: 0;
+          }
+        }
+
+      `}</style>
     </div>
   )
 }
