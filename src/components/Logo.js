313d9f5be5f7f631f079c1eb078f5c8971f06553
Allen Hai <hai@growinfinitely.com>
2017-11-15 16:12:54 -0600

    hard coded logo path to /wp-content

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import logo from '../images/asp_transparent_circle_bg_silver_stroke.png'
+
 
 function Logo ({height}) {
   return (
     <div>
-    <img src={logo}
+    <img src="/wp-content/uploads/2017/11/asp_transparent_circle_bg_silver_stroke.png"
          height={height}
          alt='ammar-selo-photography-logo' />
     <style jsx>{`
